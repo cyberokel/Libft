c3e767e9e2511b567d77347cad087f4f105cb019
Abdellatif Khalidi <akhalidi@fu-r10-p8.s19.be>
2025-11-18 17:57:06 +0100

    join

---
+++ ft_strjoin.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akhalidi <akhalidi@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/18 17:23:02 by akhalidi          #+#    #+#             */
+/*   Updated: 2025/11/18 17:56:37 by akhalidi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	 *ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int	i;
+	int	j;
+	int	len;
+
+	i = 0;
+	j = 0;
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = malloc(sizeof(char) * len);
+	if (!str)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
