14b4d1b38f56de0cc5a7ab9e0bb08ba2378a99a3
akhalidi <akhalidi@gmail.com>
2025-11-23 22:39:35 +0100

    split done

--- ft_split.c
+++ ft_split.c
@@ -12,68 +12,61 @@
 
 int	ft_count_words(char const *s, char c)
 {
-	size_t	i;
-	size_t	count;
+	int	i;
+	int	count;
 
 	i = 0;
 	count = 0;
-	if (s[0] != c)
-		count++; 
 	while(s[i])
 	{
-		if (s[i] != c && s[i - 1] == c)
+		if (s[i] != c && (i == 0 || s[i - 1] == c))
 			count++;
 		i++;
 	}
 	return (count);
 }
 
-int	ft_count_len(char const *s, char c)
-{
-	int	i;
-	while (s[i])
-	{
-		if (s[i] == c)
-			i++;
-		count++;
-		i++;
-	}
-	return (count);
-}
-
-/*int	ft_check(char const *s, char c)
+int	ft_len(char const *s, char c)
 {
 	int	i;
 
 	i = 0;
-	while (s[i] && */
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**table;
-	size_t	i;
-	size_t	words;
-	size_t	len;
+	int	i;
+	int	j;
+	int	k;
+	int	words;
 
 	i = 0;
-	words = ft_count_words(s, c);
-	len = ft_count_len(s, c);
-	table = malloc(sizeof(char*)* words + 1);
+	j = 0;
+	k = 0;
+	words = ft_count_words(s, c) + 1;
+	table = malloc(sizeof(char*)* words);
 	if (!table)
 		return (NULL);
-	*table = (sizeof(char) * len + 1);
-	if (!*table)
-		return (NULL);
-	while (table[i][j])
+	while (s[k])
 	{
+		while (s[k] == c)
+			k++;
+		table[i] = malloc(sizeof(char)  * (ft_len(&s[k], c) + 1));
+		if (!table[i])
+			return (NULL);
 		j = 0;
-		while (table[i][j] && s[k] != c)
+		while(s[k] != c)
 		{
 			table[i][j] = s[k];
 			j++;
 			k++;
 		}
 		table[i][j] = '\0';
-		i++
+		i++;
 	}
 	return (table);
 }
