f51e6fd07733f9485546b09b6ac7a813dff186c5
Abdellatif Khalidi <akhalidi@mi-r3-p3.s19.be>
2026-03-13 19:42:10 +0100

    loaded

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -1,13 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akhalidi <akhalidi@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/23 16:34:35 by akhalidi          #+#    #+#             */
-/*   Updated: 2026/02/23 16:34:39 by akhalidi         ###   ########.fr       */
+/*   Created: 2026/03/12 22:16:47 by akhalidi          #+#    #+#             */
+/*   Updated: 2026/03/13 18:08:07 by akhalidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int    ft_lstsize(t_list *lst)
+{
+	if(!lst)
+		return (!NULL);
+	int    count;
 
+	count = 0;
+	while(lst != NULL)
+	{
+		lst = lst->next;
+		count++;
+	}
+	return (count);
+}
