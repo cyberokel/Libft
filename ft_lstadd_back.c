21977a702e180c9a9ac9c674b8c39616d4d4a1f3
Abdellatif Khalidi <akhalidi@shi-r2-p1.s19.be>
2026-03-15 16:54:44 +0100

    load

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,8 +6,36 @@
 /*   By: akhalidi <akhalidi@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 21:47:59 by akhalidi          #+#    #+#             */
-/*   Updated: 2026/03/14 22:23:33 by akhalidi         ###   ########.fr       */
+/*   Updated: 2026/03/15 16:54:27 by akhalidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*ptr;
+
+	ptr = *lst;
+	ptr = ft_lstlast(ptr);
+	ptr->next = new;
+}
+
+int	main()
+{
+	t_list *n1;
+	t_list *n2;
+	t_list *n3;
+	t_list **lst;
+
+	*lst = NULL;
+	*n1 = ft_lstnew("first");
+	*n2 = ft_lstnew("deux");
+	*n3 = ft_lstnew("third");
+
+	ft_lstadd_back(lst, n1);
+	ft_lstadd_back(lst, n2);
+	ft_lstadd_back(lst, n3);
+
+	return (0);
+}
